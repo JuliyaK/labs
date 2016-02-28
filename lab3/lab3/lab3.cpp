d5f42caced100f9737e220bfb1758850fc461777
JuliyaK <y.kashlakova@yandex.ru>
2016-02-28 16:16:20 +0600

    lab3

--- lab3/lab3/lab3.cpp
+++ lab3/lab3/lab3.cpp
@@ -8,28 +8,35 @@ using namespace std;
 
 int main()
 {
-	int a, b, c, x, x1, x2, D;
+	int a, b, c, x, y;
+	double x1, x2, D;
+
 	cout << "enter a:\n";
 	cin >> a;
 	cout << "enter b:\n";
 	cin >> b;
 	cout <<  "enter c:\n";
 	cin >> c;
-		D = b * b - 4 * a * c;
-		if (D > 0)
-		{
-			x1 = (-b - sqrt(D)) / (2 * a);
-			x2 = (-b + sqrt(D)) / (2 * a);
-		}
-		if (D = 0)
-		{
-			x1 = -b / (2 * a);
-			x2 = -b / (2 * a);
-		}
-		if (D < 0)
-		{
-			cout << "корней нет";
-		}
+	cout << "enter x:\n";
+	cin >> x;
+	D = b * b - 4 * a * c;
+	if (D > 0)
+	{
+		x1 = (-b - sqrt(D)) / (2 * a);
+		x2 = (-b + sqrt(D)) / (2 * a);
+	}
+	if (D = 0)
+	{
+		x1 = -b / (2 * a);
+		x2 = -b / (2 * a);
+	}
+	if (D < 0)
+	{
+		cout << "корней нет";
+	}
+	y = (x - x1)*(x - x2);
+	cout << "y = " << y << endl;
+	getchar();
 	getchar();
 	return 0;
 }
