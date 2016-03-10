b9b6f280fe8fc75ea931998307a5d69e0bdbb845
JuliyaK <y.kashlakova@yandex.ru>
2016-03-10 21:50:35 +0600

    Lab 18

---
+++ Lab 18/Lab 18/Lab 18.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+	float i;
+	float a[20];
+	int count = 0;
+
+	cout << "Array" << endl;
+	for (i = 0; i < 20; i++) {
+		a[i] = rand() % 41 - 10;
+		cout << a[i] << " ";
+		if (a[i] % 5 == 0) {
+			count++;
+		}
+	}
+	cout << endl;
+	cout << "Result: " << count << endl;
+	getchar();
+	return 0;
+}
