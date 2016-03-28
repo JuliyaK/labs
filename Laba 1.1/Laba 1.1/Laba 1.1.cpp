9868c765b5216e4f9af3b17bf03c5f863ce966fa
JuliyaK <y.kashlakova@yandex.ru>
2016-03-28 21:09:29 +0600

    Laba 1.1

--- Laba 1.1/Laba 1.1/Laba 1.1.cpp
+++ Laba 1.1/Laba 1.1/Laba 1.1.cpp
@@ -21,17 +21,19 @@ int main()
 	{
 		x1 = ((-b + sqrt(D)) / (2 * a));
 		x2 = ((-b - sqrt(D)) / (2 * a));
-		cout << x1 << x2 << endl;
+		cout << "x1 = " << x1 << endl;
+		cout << "x2 = " << x2 << endl;
 	}
 	if (D == 0)
 	{
 		x1 = ((-b) / (2 * a));
-		cout << x1 << endl;
+		cout << "x1 = " << x1 << endl;
 	}
 	if (D < 0)
 	{
 		cout << "resheni net" << endl;
 	}
+	getchar();
 	getchar();
 	return 0;
 }
